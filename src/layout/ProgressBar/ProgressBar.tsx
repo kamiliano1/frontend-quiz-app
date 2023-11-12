46f78a032072d4c17a8e1bcfee41973e0e253d7f
kamiliano1 <108863365+kamiliano1@users.noreply.github.com>
2023-11-12 12:21:14 +0100

    Install and create ProgressBar

---
+++ src/layout/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import * as Progress from "@radix-ui/react-progress";
+import { currentThemeState } from "@/atoms/themeSwitcherAtom";
+import { useRecoilValue } from "recoil";
+type ProgressBarProps = {};
+
+const ProgressBar: React.FC<ProgressBarProps> = () => {
+  const activeTheme = useRecoilValue(currentThemeState);
+  const [progress, setProgress] = useState(0);
+
+  return (
+    <div
+      className={`rounded-full p-1 ${
+        activeTheme.isDarkMode ? "bg-navy" : "bg-white"
+      }`}
+    >
+      <Progress.Root
+        max={10}
+        aria-label="questions progress bar"
+        className={`relative overflow-hidden rounded-full w-full h-[8px]`}
+        style={{
+          transform: "translateZ(0)",
+        }}
+        value={progress}
+      >
+        <Progress.Indicator
+          className={`bg-purple rounded-[104px] h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]`}
+          style={{
+            transform: `translateX(-${100 - progress * 10}%)`,
+          }}
+        />
+      </Progress.Root>
+    </div>
+  );
+};
+export default ProgressBar;
